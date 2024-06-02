0963a55f86439a3811ce340de3d24bb405b27aaa
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-02 19:10:58 +0000

    chore: Update dependencies to include queues 1.0.2

--- expand_string/src/main.rs
+++ expand_string/src/main.rs
@@ -1,14 +1,20 @@
-
 // https://leetcode.com/problems/decode-string/
 // Input: s = "2[abc]3[cd]ef"
 // Output: "abcabccdcdcdef"
+
+
+use queues::*;
+
 fn main() {
+    let queue:Queue<isize>=queue![];
+
     println!("{}",repeat_string(4, "ef"));
+
 }
 
 fn repeat_string(times:u32,to_be_repeated:&str)-> String{
     let mut window:String=String::from("");
-    for num in (0..times){
+    for _num in 0..times {
         window+=to_be_repeated;
     }
     window
